b1ff724ae2a70b51cff5b88ecac4288812b39e87
Zhiqiang <494146653@qq.com>
2024-12-10 17:03:01 +0800

    enable CORS

--- realtime-sample/main.py
+++ realtime-sample/main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 from realtime import handle as handle_realtime
 
@@ -7,6 +8,15 @@ load_dotenv(override=True)
 
 app = FastAPI()
 app.add_middleware(HTTPSRedirectMiddleware)
+
+# 允许特定的来源进行跨域请求
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 可以指定允许的来源
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
